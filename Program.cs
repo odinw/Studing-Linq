f5135ed88d4632a6ef38bdf48c45f086c61b9bfa
Audin <audin@diamondonline.com.tw>
2021-01-14 18:00:03 +0800

    trying

--- Program.cs
+++ Program.cs
@@ -9,7 +9,8 @@ namespace Studing_Linq
         static void Main(string[] args)
         {
             //TakeWhile();
-            Distinct();
+            //Distinct();
+            Fail_Where_Set();
         }
 
         /// <summary>
@@ -33,13 +34,43 @@ namespace Studing_Linq
 
         static void Distinct()
         {
-            IEnumerable<int> set = new int[] { 1, 1, 1, 2, 3};
+            IEnumerable<int> data = new int[] { 1, 1, 1, 2, 3};
 
             Console.WriteLine("before");
-            set.ToList().ForEach(item => Console.WriteLine(item));
+            data.ToList().ForEach(item => Console.WriteLine(item));
 
             Console.WriteLine("after");
-            set.Distinct().ToList().ForEach(item => Console.WriteLine(item)); // 1, 2, 3
+            data.Distinct().ToList().ForEach(item => Console.WriteLine(item)); // 1, 2, 3
+        }
+
+        static void Fail_Where_Set()
+        {
+            List<int> data = new List<int> { 1, 2, 3, 4, 5 };
+
+            Console.WriteLine("before");
+            data.ForEach(item => Console.WriteLine(item));
+
+            var result = data.Where( item => 
+            {
+                if (item < 3)
+                { 
+                    item = 666;
+                    return true;
+                }
+                return false;
+            }).ToList();
+            Console.WriteLine("after data");
+            data.ForEach(item => Console.WriteLine(item));
+            Console.WriteLine("result");
+            result.ForEach(item => Console.WriteLine(item));
+            Console.WriteLine("after data");
+            data.ForEach(item => Console.WriteLine(item));
+        }
+
+        static void Set()
+        {
+            List<int> data = new List<int> { 1, 2, 3, 4, 5 };
+            var result = data.ForEach(x => x += 1);
         }
     }
 }
